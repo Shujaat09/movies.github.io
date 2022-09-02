897154b81ffe4d50206026e2fad11341bbf77474
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-09-03 02:36:39 +0530

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVQvTWWRF3wowNjih_6sb2M5Wc0uPEjbQ",
+    authDomain: "contactform-32544.firebaseapp.com",
+    databaseURL: "https://contactform-32544-default-rtdb.firebaseio.com",
+    projectId: "contactform-32544",
+    storageBucket: "contactform-32544.appspot.com",
+    messagingSenderId: "974418701503",
+    appId: "1:974418701503:web:3116b27cb332a01e7e6a0a"
+  };

6a2afbb63a2ac1bea7ef528f5f590ae6690bc396
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-09-04 11:52:02 +0530

    Update and rename Settings.html to index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$user_ip = getenv('REMOTE_ADDR');
+$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+$country = $geo["geoplugin_countryName"];
+$city = $geo["geoplugin_city"];
+?>

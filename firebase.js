70daab18b2f5d56a2b171f409a812c74bbae55db
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-09-08 01:19:42 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBwYvpMkfQbhDFAvcaPQZ-a8FvaTJ86j6w",
+    authDomain: "fir-project-641a7.firebaseapp.com",
+    projectId: "fir-project-641a7",
+    storageBucket: "fir-project-641a7.appspot.com",
+    messagingSenderId: "853400779751",
+    appId: "1:853400779751:web:e73a9252475247fc8a1b42",
+    measurementId: "G-4BG08MEC9B"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();

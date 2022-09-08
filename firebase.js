647230f740cb377227513624b435bd54956c31a3
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-09-08 20:31:12 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCfFCv2Jpb0Tp2fTRhdHRTBNGWzWouCe1E",
+     apiKey: "AIzaSyCfFCv2Jpb0Tp2fTRhdHRTBNGWzWouCe1E",
     authDomain: "shujaatcc-d84b9.firebaseapp.com",
     projectId: "shujaatcc-d84b9",
     storageBucket: "shujaatcc-d84b9.appspot.com",
     messagingSenderId: "907751414405",
-    appId: "1:907751414405:web:cf0d9075b9d9303de6a7ec",
+    appId: "1:907751414405:web:fb32fee3bb892139e6a7ec",
+    measurementId: "G-TFRLR3G6FE"
     measurementId: "G-J21D0XH4XE"
 };
 // Initialize Firebase

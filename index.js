bd28059e492cccb2ad0bca4dda0f7fa1622afd89
Shujaat09 <112425715+Shujaat09@users.noreply.github.com>
2022-09-08 20:17:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit",(event)=>{
 
 firebase.auth().onAuthStateChanged((user)=>{
     if(user){
-        location.replace("welcome.html")
+        location.replace("Configure.html")
     }
 })
 
